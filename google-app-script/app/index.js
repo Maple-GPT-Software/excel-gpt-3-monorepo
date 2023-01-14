3089782234d4baed02ec16fcdd39003a49cc3d9c
tomzacchia <tomzacchia@live.com>
2023-01-14 16:47:21 -0500

    add: app-script

---
+++ google-app-script/app/index.js
@@ -0,0 +1,8 @@
+// import { render } from 'preact';
+// import { h, useState } from 'preact/hooks';
+
+function App() {
+  return <h1> hello world </h1>;
+}
+
+preact.render(<App />, document.body);
