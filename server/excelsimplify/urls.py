1f4773dee242729e42a6e956bfe1f04bbabea02d
Mimi Ta <mimi_ta3000@hotmail.com>
2023-02-02 02:57:17 -0500

    Scrap swagger

--- server/excelsimplify/urls.py
+++ server/excelsimplify/urls.py
@@ -1,24 +1,9 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 from . import views
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Excel Simplify",
-        default_version='v1',
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
     path('', csrf_exempt(views.completion), name='completion'),
-    path('swagger/', schema_view.with_ui('swagger',
-                                         cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
 ]
