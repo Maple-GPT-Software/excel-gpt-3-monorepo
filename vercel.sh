7379286209168d8748da1e663fa3418bfec07eb8
tomzacchia <tomzacchia@live.com>
2023-02-18 23:30:09 -0500

    ci: vercel bash instructions

---
+++ vercel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# only deploy for changes to ./webapp
+git diff HEAD^ HEAD --quiet ./webapp
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+# allow staging deployments for branches that start with staging/
+# and main
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging/*" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
