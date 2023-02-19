4e367974d9bb86e7fce72af8d5040c94c2b4f0c2
tomzacchia <tomzacchia@live.com>
2023-02-18 23:37:11 -0500

    ci: actually build

--- webapp/vercel.sh
+++ webapp/vercel.sh
@@ -10,6 +10,7 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 if [[ "$VERCEL_GIT_COMMIT_REF" == "staging/*" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
+    npm run build
 
 else
   # Don't build
