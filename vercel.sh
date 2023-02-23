9d630c516c59290ad59cbe85fa021e1157f9fd9e
tomzacchia <tomzacchia@live.com>
2023-02-22 23:44:24 -0500

    ci: move bash script

--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE" 
 
 # only deploy for changes to ./webapp
 git diff HEAD^ HEAD --quiet .
