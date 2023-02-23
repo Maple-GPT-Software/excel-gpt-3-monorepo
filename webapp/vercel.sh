d7101cd566586ffcab311d28df097c294e877171
tomzacchia <tomzacchia@live.com>
2023-02-22 23:39:22 -0500

    ci: preview all commits to webapp

--- webapp/vercel.sh
+++ webapp/vercel.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# only deploy for changes to ./webapp
-git diff HEAD^ HEAD --quiet ./webapp
-
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-# allow staging deployments for branches that start with staging/
-# and main
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging/*" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  # Proceed with the build
-    echo "✅ - Build can proceed"
-    npm run build
-    exit 0;
+# only deploy for changes to ./webapp
+git diff HEAD^ HEAD --quiet .
 
-else
-  # Don't build
-  echo "🛑 - Build cancelled"
-  exit 0;
-fi
