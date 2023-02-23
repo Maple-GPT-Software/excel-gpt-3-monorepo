70de4dd54ffa9ac98e0a4c39a717491c6ce84715
tomzacchia <tomzacchia@live.com>
2023-02-23 00:00:55 -0500

    ci: change root dir in vercel UI

--- vercel.sh
+++ vercel.sh
@@ -4,4 +4,4 @@ echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE" 
 
 # only deploy for changes to ./webapp
-git diff --quiet HEAD^ HEAD -- .
+git diff HEAD^ HEAD --quiet ./webapp
